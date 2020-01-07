e17177cf3bdd5f89e37528e317868a2cf398f9b3
user162958 <user162958@as412.local>
2020-01-07 17:33:59 +0800

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by user162958 on 1/7/20.
+//  Copyright © 2020 user162958. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
